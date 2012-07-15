83ea7dc19312a0cd1d5644cab530231859313302
Anand Gupta <spartango@gmail.com>
2012-07-15 14:28:29 -0400

    making progress on map

--- src/com/spartango/jediscollect/collections/JedisMap.java
+++ src/com/spartango/jediscollect/collections/JedisMap.java
@@ -1,65 +1,129 @@
 package com.spartango.jediscollect.collections;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
 
-public class JedisMap<K, V> extends JedisBackedObject implements Map<K, V> {
+public class JedisMap<K extends Serializable, V extends Serializable> extends
+                                                                      JedisBackedObject implements
+                                                                                       Map<K, V> {
 
     public JedisMap(String key, JedisPool pool) {
         super(key, pool);
-        // TODO Auto-generated constructor stub
     }
 
     public JedisMap(String key) {
         super(key);
-        // TODO Auto-generated constructor stub
     }
 
     @Override public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        Jedis jedis = pool.getResource();
+        long value = 0;
+        try {
+            value = jedis.hlen(key);
+        } finally {
+            pool.returnResource(jedis);
+        }
+        return (int) value;
     }
 
     @Override public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return size() == 0;
     }
 
-    @Override public boolean containsKey(Object key) {
-        // TODO Auto-generated method stub
-        return false;
+    @Override public boolean containsKey(Object tkey) {
+
+        Jedis jedis = pool.getResource();
+        boolean value = false;
+        try {
+            value = jedis.hexists(key.getBytes(), serializeObject(tkey));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            pool.returnResource(jedis);
+        }
+        return value;
     }
 
     @Override public boolean containsValue(Object value) {
-        // TODO Auto-generated method stub
-        return false;
+        // Redis doesnt have an easy way to do this
+        throw new UnsupportedOperationException();
     }
 
-    @Override public V get(Object key) {
-        // TODO Auto-generated method stub
-        return null;
+    @Override public V get(Object tkey) {
+        Jedis jedis = pool.getResource();
+        V value = null;
+        try {
+            byte[] object = jedis.hget(key.getBytes(), serializeObject(tkey));
+            value = (V) deserialize(object);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.returnResource(jedis);
+        }
+        return value;
     }
 
-    @Override public V put(K key, V value) {
-        // TODO Auto-generated method stub
-        return null;
+    @Override public V put(K tkey, V value) {
+        Jedis jedis = pool.getResource();
+        try {
+            jedis.hset(key.getBytes(),
+                       serializeObject(tkey),
+                       serializeObject(value));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.returnResource(jedis);
+        }
+        return value;
     }
 
-    @Override public V remove(Object key) {
-        // TODO Auto-generated method stub
-        return null;
+    @Override public V remove(Object tkey) {
+        Jedis jedis = pool.getResource();
+        V value = null;
+        try {
+            Transaction transaction = jedis.multi();
+            Response<byte[]> object = transaction.hget(key.getBytes(),
+                                                       serializeObject(tkey));
+            transaction.hdel(key.getBytes(), serializeObject(tkey));
+            transaction.exec();
+
+            value = (V) deserialize(object.get());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            pool.returnResource(jedis);
+        }
+        return value;
     }
 
     @Override public void putAll(Map<? extends K, ? extends V> m) {
         // TODO Auto-generated method stub
-
+        // For each item in the map
+        // Prep each by serializing
+        // Put all of them
     }
 
     @Override public void clear() {
         // TODO Auto-generated method stub
+        // Get all the keys
+        // Delete all of them
 
     }
 
@@ -76,6 +140,50 @@ public class JedisMap<K, V> extends JedisBackedObject implements Map<K, V> {
     @Override public Set<java.util.Map.Entry<K, V>> entrySet() {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private static byte[] serializeObject(Object tkey) throws IOException {
+        // Serialize the key
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = new ObjectOutputStream(bos);
+        out.writeObject(tkey);
+        byte[] byteKey = bos.toByteArray();
+        out.close();
+        bos.close();
+        return byteKey;
+    }
+
+    public static Object
+            deserialize(InputStream inputStream) throws IOException {
+        if (inputStream == null) {
+            throw new IllegalArgumentException("The InputStream must not be null");
+        }
+        ObjectInputStream in = null;
+        try {
+            // stream closed in the finally
+            in = new ObjectInputStream(inputStream);
+            return in.readObject();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new IOException();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+    }
+
+    public static Object deserialize(byte[] objectData) throws IOException {
+        if (objectData == null) {
+            throw new IllegalArgumentException("The byte[] must not be null");
+        }
+        ByteArrayInputStream bais = new ByteArrayInputStream(objectData);
+        return deserialize(bais);
     }
 
 }
--- src/com/spartango/jediscollect/collections/JedisQueue.java
+++ src/com/spartango/jediscollect/collections/JedisQueue.java
@@ -19,12 +19,10 @@ public class JedisQueue<T> extends JedisBackedObject implements
     }
 
     @Override public int size() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     @Override public boolean isEmpty() {
-        // TODO Auto-generated method stub
         return false;
     }
 
