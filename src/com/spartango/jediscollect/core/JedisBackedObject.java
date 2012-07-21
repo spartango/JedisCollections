ee56b720348db36894c29701e65faa92c553895f
Anand Gupta <spartango@gmail.com>
2012-07-21 11:01:01 -0400

    moved to Kryo

--- src/com/spartango/jediscollect/core/JedisBackedObject.java
+++ src/com/spartango/jediscollect/core/JedisBackedObject.java
@@ -4,11 +4,12 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 
 import redis.clients.jedis.JedisPool;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 
 public abstract class JedisBackedObject {
 
@@ -17,6 +18,9 @@ public abstract class JedisBackedObject {
     // Key under which we can find this object
     protected final String   key;
     protected final byte[]   byteKey;
+
+    // Serializer
+    protected final Kryo     kryo;
 
     // Pool of connections on which we can perform operations
     protected JedisPool      pool;
@@ -30,6 +34,7 @@ public abstract class JedisBackedObject {
         this.key = key;
         this.byteKey = key.getBytes();
         this.pool = pool;
+        this.kryo = new Kryo();
     }
 
     public String getKey() {
@@ -54,43 +59,26 @@ public abstract class JedisBackedObject {
         JedisBackedObject.defaultPool = defaultPool;
     }
 
-    protected static byte[] serializeObject(Object tkey) throws IOException {
+    protected byte[] serializeObject(Object tkey) throws IOException {
         // Serialize the key
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutput out = new ObjectOutputStream(bos);
-        out.writeObject(tkey);
-        byte[] byteKey = bos.toByteArray();
-        out.close();
-        bos.close();
-        return byteKey;
-    }
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             Output out = new Output()) {
 
-    protected static Object
-            deserialize(InputStream inputStream) throws IOException {
-        if (inputStream == null) {
-            ObjectInputStream in = null;
-            try {
-                in = new ObjectInputStream(inputStream);
-                return in.readObject();
+            kryo.writeClassAndObject(out, tkey);
 
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-                throw new IOException();
-            } finally {
-                try {
-                    if (in != null) {
-                        in.close();
-                    }
-                } catch (IOException e) {
-                    // Don't really care if the close failed
-                }
-            }
-        } else {
-            return null;
+            byte[] byteKey = bos.toByteArray();
+            return byteKey;
         }
     }
 
-    protected static Object deserialize(byte[] objectData) throws IOException {
+    protected Object deserialize(InputStream inputStream) throws IOException {
+        try (Input input = new Input(inputStream)) {
+            Object object = kryo.readClassAndObject(input);
+            return object;
+        }
+    }
+
+    protected Object deserialize(byte[] objectData) throws IOException {
         if (objectData == null) {
             throw new IllegalArgumentException("The byte[] must not be null");
         }
